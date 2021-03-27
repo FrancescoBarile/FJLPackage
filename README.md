91110956ef56ffdd3951687a855fd8ecb224934e
Francesco Barile <f.barile2@campus.unimib.it>
2021-03-27 15:35:30 +0100

    Adding cpp functions to src folder

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# cpp
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of cpp is to ...
+
+## Installation
+
+You can install the released version of cpp from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("cpp")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(cpp)
+## basic example code
+```
+
---
+++ src/betahat.cpp
@@ -0,0 +1,56 @@
+#include "RcppArmadillo.h"
+
+using namespace Rcpp;
+
+//' @title Estimation Linear Regression model via Gradient Descend method
+//' @name betahat_GD_Cpp
+//' @param beta initial guess beta vector
+//' @param X design matrix
+//' @param Y response variable vector
+//' @param tolerance level
+//' @param maxit max num of iter
+//' @param stepsize learning parameter
+//' @export
+
+// [[Rcpp::export]]
+
+arma::vec betahat_GD_Cpp(arma::vec beta, arma::mat X, arma::vec Y, 
+                   double tolerance, int maxit, double stepsize) {
+  int t = 1;
+  double error = 1;
+  
+  while( (error>tolerance) & (t<=maxit) ){
+    arma::vec gr =  -2*X.t()*Y + 2*X.t()*X*beta;
+    arma::vec beta_old = beta;
+    beta = beta - stepsize*gr; 
+    error = max(abs(beta-beta_old));
+    t = t + 1;
+  }
+  return beta;
+}
+
+//' @title Estimation Linear Regression model via Steepest Descend method
+//' @name betahat_SD_Cpp
+//' @param beta initial guess beta vector
+//' @param X design matrix
+//' @param Y response variable vector
+//' @param tolerance level
+//' @export
+// [[Rcpp::export]]
+
+arma::vec betahat_SD_Cpp(arma::vec beta, arma::mat X, arma::vec Y, double tolerance) {
+  
+  arma::mat H = 2*X.t()*X; 
+  double error = 1;
+  
+  while( error>tolerance ){
+    arma::vec gr =  -2*X.t()*Y + 2*X.t()*X*beta;
+    arma::vec beta_old = beta;
+    double numerator = norm(gr, 2);
+    double denominator = as_scalar(gr.t()*H*gr);
+    double step = pow(numerator, 2.0)/denominator;
+    beta = beta - step*gr; 
+    error = max(abs(beta-beta_old));
+  }
+  return beta;
+}
