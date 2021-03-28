0f4dc47405a30f18ddba21e7a144aa8870cf64e7
Francesco Barile <f.barile2@campus.unimib.it>
2021-03-28 12:19:52 +0200

    adding example with mtcars on vignette

--- tests/testthat/test-lm_specification.R
+++ tests/testthat/test-lm_specification.R
@@ -7,3 +7,14 @@ test_that("lm.model_spec works for variables name", {
 test_that("lm.model_spec works for intercept", {
   expect_equal(lm.model_spec(Dep.var ~ ., mydataset)$intercept, TRUE)
 })
+
+
+test_that("lm.model_spec works for variables name on mtcars", {
+  expect_equal(lm.model_spec(mpg ~ hp + wt -1, mtcars)$var_names, 
+               tibble(type=c('Dependent', 'Regressor 1', 'Regressor 2'),
+                      var_name=c('mpg', 'hp', 'wt')))
+})
+
+test_that("lm.model_spec works for intercept on mtcars", {
+  expect_equal(lm.model_spec(mpg ~ hp + wt -1, mtcars)$intercept, FALSE)
+})
