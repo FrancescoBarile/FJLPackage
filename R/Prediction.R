18475fb93a106fd6819033048a0de4a5b32e1828
Jiefeng Bi <623101934@qq.com>
2021-04-08 22:30:42 +0200

    prediction revision

--- R/Prediction.R
+++ R/Prediction.R
@@ -6,13 +6,13 @@
 #' @export
 #' @import dplyr
 #' @import stringr
-
-y_pred <- function(opt_obj) {
-  method = str_c(str_extract_all(opt_obj$method, "[A-Z]")[[1]][1], 
-                 str_extract_all(opt_obj$method, "[A-Z]")[[1]][2])
-  beta_hat = opt_obj$beta_hat
-  X = as.matrix(opt_obj$X)
-  Y_hat = tibble(y_hat=as.numeric(X%*%beta_hat) )%>%
-    rename(!!paste0(names(opt_obj$Y), "_pred_", method, sep="") := y_hat)
-  return(Y_hat)
-}
+  y_pred <- function(formula,opt_obj,df) {
+    method = str_c(str_extract_all(opt_obj$method, "[A-Z]")[[1]][1], 
+                   str_extract_all(opt_obj$method, "[A-Z]")[[1]][2])
+    beta_hat = opt_obj$beta_hat
+    data=data_xy(formula,df)
+    X = as.matrix(data$X)
+    Y_hat = tibble(y_hat=as.numeric(X%*%beta_hat) )%>%
+      rename(!!paste0(names(opt_obj$Y), "_pred_", method, sep="") := y_hat)
+    return(Y_hat)
+  }
